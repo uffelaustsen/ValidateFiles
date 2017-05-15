4bfbe1603f33bf6477b7db94faf63ec0b0c0c1d2
Uffe Laustsen <uffe.laustsen@gmail.com>
2017-05-15 12:38:08 +0200

    Add project files.

---
+++ ValidateFiles/DataGridHelper.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ValidateFiles
+{
+    static class DataGridHelper
+    {
+
+        public static void SetColumnWidths(DataGridView dataGridView)
+        {
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+            {
+                if (column.Index % 2 != 0)
+                {
+                    dataGridView.Columns[column.Index].Visible = false;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty1")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "beløb")
+                {
+                    dataGridView.Columns[column.Index].Width = 50;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty2")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "adresse")
+                {
+                    dataGridView.Columns[column.Index].Width = 125;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "postnr")
+                {
+                    dataGridView.Columns[column.Index].Width = 45;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "telefon2")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "email")
+                {
+                    dataGridView.Columns[column.Index].Width = 200;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty3")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "regnr")
+                {
+                    dataGridView.Columns[column.Index].Width = 45;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty4")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty5")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty6")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty7")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty8")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty10")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+                if (dataGridView.Columns[column.Index].HeaderText.ToLower() == "empty11")
+                {
+                    dataGridView.Columns[column.Index].Width = 0;
+                }
+
+            }
+
+        }
+
+        public static void ColorCellsWithValidationErrors(DataGridView dataGridView)
+        {
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                try
+                {
+                    foreach (DataGridViewColumn col in dataGridView.Columns)
+                    {
+                        if (col.Index % 2 != 0)
+                        {
+                            string cellValue = dataGridView.Rows[row.Index].Cells[col.Index].Value.ToString();
+                            if (cellValue.ToLower() == "true")
+                            {
+                                dataGridView.Rows[row.Index].Cells[col.Index - 1].Style.BackColor = Color.Red;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ ValidateFiles/ValidationHelper.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ValidateFiles
+{
+    public static class ValidationHelper
+    {
+        public static bool IsEmailValid(string email)
+        {
+
+            bool isEmail = Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+            return isEmail;
+        }
+
+        public static bool DoesAddressValidatOK(string address)
+        {
+            bool isAddressOK = false;
+            var requestUri = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}&sensor=false", Uri.EscapeDataString(address));
+
+            var request = WebRequest.Create(requestUri);
+            var response = request.GetResponse();
+            var xdoc = XDocument.Load(response.GetResponseStream());
+
+            var result = xdoc.Element("GeocodeResponse").Element("result");
+            var locationElement = result.Element("geometry").Element("location");
+            var lat = locationElement.Element("lat");
+            var lng = locationElement.Element("lng");
+
+            string status = (string)
+                (from el in xdoc.Descendants("status")
+                 select el).First();
+
+            string formattedAddress = (string)
+                (from el in xdoc.Descendants("formatted_address")
+                 select el).First();
+
+            foreach (XElement e in xdoc.Descendants("status"))
+            {
+                Console.WriteLine("Descendants : " + e.Value);
+                if ((e.Value == "OK") && (formattedAddress.ToLower() == address.ToLower()))
+                {
+                    isAddressOK = true;
+                }
+                else
+                {
+                    isAddressOK = false;
+                }
+            }
+
+            return isAddressOK;
+
+
+        }
+
+        public enum CprValidity { Valid, MaybeValid, Invalid }
+
+        public static CprValidity IsCprValid(string cpr)
+        {
+
+            if (cpr == null)
+                throw new ArgumentNullException();
+
+            if(cpr.Contains("-") == false)
+                return CprValidity.Invalid;
+
+
+            cpr = cpr.Replace("-", "");
+
+            if (cpr.Length != 10)
+                return CprValidity.Invalid;
+
+            int sum = 0;
+            int[] check = new int[] { 4, 3, 2, 7, 6, 5, 4, 3, 2, 1 };
+            try
+            {
+                for (int i = 0; i < check.Length; i++)
+                    sum += int.Parse(cpr.Substring(i, 1)) * check[i];
+            }
+            catch (FormatException)
+            {
+                return CprValidity.Invalid;
+            }
+
+            if (sum % 11 == 0)
+                return CprValidity.Valid;
+            else
+                return CprValidity.MaybeValid;
+        }
+
+        public static bool DoesRegnrValidatOK(string regnr)
+        {
+            if ((regnr.Length == 4) && (regnr.Substring(0, 4) != "4571"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool DoesKontonrValidatOK(string kontonr)
+        {
+            if (kontonr.Length <= 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string MakeStringPascalCase(string stringToChange)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            return textInfo.ToTitleCase(stringToChange);
+
+        }
+
+        public static string RemoveDashAndDotFromString(string stringToChange)
+        {
+            string result = stringToChange.Replace("-", "");
+            result = result.Replace(".", "");
+            result = result.Replace("00:00:00", "");
+            return result;
+
+        }
+
+        //public static string MakeStringUTF8Encoded(string stringToEncode)
+        //{
+        //    byte[] data = Encoding.GetEncoding(1252).GetBytes(stringToEncode);
+        //    return Encoding.UTF8.GetString(data);
+        //}
+
+        //public static string GetØÆFromWeirdCharacters(string stringReplaceCharactersIn)
+        //{
+        //    string result;
+        //    result = stringReplaceCharactersIn.Replace("ã¥", "å");
+        //    result = stringReplaceCharactersIn.Replace("ã¦", "æ");
+        //    return result;
+        //}
+
+        public static bool DoesPhoneNumberValidatOK(string phoneNumber, bool blankPhoneNumberAllowed)
+        {
+            if (blankPhoneNumberAllowed == false)
+            {
+                if ((phoneNumber == null) || (phoneNumber.Length == 0))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if ((phoneNumber == null) || (phoneNumber.Length == 0))
+                {
+                    return true;
+                }
+            }
+
+
+            if (phoneNumber.Contains("+45"))
+            {
+                if (phoneNumber.Length == 11)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (phoneNumber.Length == 8)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            }
+        }
+    }
+
